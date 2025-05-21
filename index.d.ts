44dd759d0bfae8bf2657a09218cbaa734baf97d0
Danyyal ali yar <49082161+danyyal@users.noreply.github.com>
2025-05-21 18:18:15 +0500

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Validate an email address.
+ * @param {string} email - The email address to validate.
+ * @returns {boolean}
+ */
+export function validate(email: string): boolean;
+export const tester: RegExp;
